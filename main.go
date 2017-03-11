93f864c4ae4af5a9c9f5690c5204e14a045466fd
Jorrit Salverda <JorritSalverda@users.noreply.github.com>
2017-03-11 14:55:13 +0100

    Updated comment

--- main.go
+++ main.go
@@ -130,7 +130,7 @@ func main() {
 			}
 		}
 
-		// sleep random time between 225 and 375 seconds
+		// sleep random time around 900 seconds
 		sleepTime := applyJitter(900)
 		fmt.Printf("Sleeping for %v seconds...\n", sleepTime)
 		time.Sleep(time.Duration(sleepTime) * time.Second)
